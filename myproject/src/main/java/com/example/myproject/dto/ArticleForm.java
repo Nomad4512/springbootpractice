cb0762615fef42e79dcba6857263786ae43358d6
Hyunbin Ban <103755593+Nomad4512@users.noreply.github.com>
2022-08-07 11:54:04 +0900

    롬복) dto리팩토링 - 생성자

--- myproject/src/main/java/com/example/myproject/dto/ArticleForm.java
+++ myproject/src/main/java/com/example/myproject/dto/ArticleForm.java
@@ -1,18 +1,15 @@
 package com.example.myproject.dto;
 
 import com.example.myproject.entity.Article;
+import lombok.AllArgsConstructor;
 import lombok.ToString;
 
     @ToString
+    @AllArgsConstructor
     public class ArticleForm {
 
     private String title;
     private String content;
-
-        public ArticleForm(String title, String content) {
-            this.title = title;
-            this.content = content;
-        }
 
         public Article toEntity() {
             return new Article(null,title,content);
