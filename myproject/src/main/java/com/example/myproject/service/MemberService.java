b02a025c3ec2cd36461dc9d4bbdbf42230dae58a
Hyunbin Ban <103755593+Nomad4512@users.noreply.github.com>
2022-08-11 00:31:10 +0900

    회원 서비스 개발

---
+++ myproject/src/main/java/com/example/myproject/service/MemberService.java
@@ -0,0 +1,41 @@
+package com.example.myproject.service;
+
+import com.example.myproject.domain.Member;
+import com.example.myproject.repository.MemberRepository;
+import com.example.myproject.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {// 서비스는 비지니스에 의존적으로 설계 -> 그래야 문제가 생겼을때 직관적
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /*
+     * 회원 가입
+     */
+    public Long join(Member member){
+        // 중복 회원 검증
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member){
+        memberRepository.findByName(member.getName()) // Optional<member>가 바로나오면 안예쁘니까
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /*
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
